3d02dc61497f931c0c00f696700f7e12e23778e4
Guirdo <produccionesaldo@gmail.com>
2023-01-08 16:37:41 -0600

    add useForm function

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-  
+import { useState } from "react"
+
+function useForm(initialState){
+  if(typeof initialState !== 'object') {
+    throw new Error('parameter should be at least an empty object')
+  }
+  if(initialState instanceof Array) {
+    throw new Error('parameter should be at least an empty object')
+  }
+
+  const [formValues, setFormValues] = useState(initialState)
+
+  const updateValues = (newState) => {
+    setFormValues(newState)
+  }
+
+  const reset = () => {
+    setFormValues(initialState)
+  }
+
+  return {formValues, updateValues, reset}
+}
+
+export default useForm
