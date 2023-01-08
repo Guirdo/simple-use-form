6fc2c18aa8d73e8e02b73845dd6156a1f5c1b4a2
Guirdo <produccionesaldo@gmail.com>
2023-01-08 15:29:31 -0600

    add testing configuration

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'main.js'),
       name: 'Simple useForm',
       fileName: 'simple-use-form',
     },
   },
+  test: {
+    reporters: ["verbose"],
+    dir: resolve(__dirname,'__test__')
+  }
 })
